4152249b26142f8a4f05e2a1b35d36c3ce2b51ab
saydaliyev1996 <saydaliyevazizxon@mail.ru>
2023-09-01 18:27:59 +0500

    responsive designer portfolio using html, css and js

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const menuBtn = document.querySelector('.menu__icon');
+const menu = document.querySelector('.menu__list');
+
+if(menuBtn && menu) {
+  menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+  });
+
+  menu.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+      menuBtn.classList.toggle('active');
+      menu.classList.toggle('active');
+    })
+  })
+}
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+anchors.forEach(anchor => {
+  anchor.addEventListener('click', e => {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute('href').substring(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    })
+  });
+});
